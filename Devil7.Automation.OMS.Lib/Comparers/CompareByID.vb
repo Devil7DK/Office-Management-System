0681eb2f89f97a5f9c43afe31d5b7c11b1f4de82
Devil7DK <dineshthangavel47@gmail.com>
2018-07-19 18:45:20 +0530

    OMS Lib: Add Custom Comparer Classes

---
+++ Devil7.Automation.OMS.Lib/Comparers/CompareByID.vb
@@ -0,0 +1,34 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Imports Devil7.Automation.OMS.Lib.Objects
+
+Namespace Comparers
+    Public Class CompareByID
+
+        Implements IComparer(Of Object)
+
+        Function Compare(ByVal x As Object, ByVal y As Object) As Integer Implements System.Collections.Generic.IComparer(Of Object).Compare
+            Return x.ID.CompareTo(y.ID)
+        End Function
+
+    End Class
+End Namespace
