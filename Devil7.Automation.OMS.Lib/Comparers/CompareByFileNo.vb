29e91a90b212c2f543e2f13476b729b50704da6b
Devil7DK <dineshthangavel47@gmail.com>
2018-07-27 18:48:46 +0530

    OMS Lib:Add Comparers for Client to Compare by - ID, Name & PAN

---
+++ Devil7.Automation.OMS.Lib/Comparers/CompareByFileNo.vb
@@ -0,0 +1,34 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Imports Devil7.Automation.OMS.Lib.Objects
+
+Namespace Comparers
+    Public Class CompareByFileNo
+
+        Implements IComparer(Of Client)
+
+        Function Compare(ByVal x As Client, ByVal y As Client) As Integer Implements System.Collections.Generic.IComparer(Of Client).Compare
+            Return x.FileNo.CompareTo(y.FileNo)
+        End Function
+
+    End Class
+End Namespace
