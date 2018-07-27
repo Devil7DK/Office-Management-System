c45fa903753ee6f0f34cb82521e7a6fa22e33481
Devil7DK <dineshthangavel47@gmail.com>
2018-07-27 15:56:00 +0530

    OMS Lib: Client - Arrange Properties

--- Devil7.Automation.OMS.Lib/Objects/Client.vb
+++ Devil7.Automation.OMS.Lib/Objects/Client.vb
@@ -22,33 +22,39 @@
 Namespace Objects
     Public Class Client
 
+        Dim ID_ As Integer = -1
+        ReadOnly Property ID As Integer
+            Get
+                Return ID_
+            End Get
+        End Property
+        Property Photo As Drawing.Image = Nothing
         Property PAN As String = ""
         Property Name As String = ""
         Property FatherName As String = ""
         Property Mobile As String = ""
         Property Email As String = ""
-        Property DOB As Date
         Property AddressLine1 As String = ""
         Property AddressLine2 As String = ""
         Property District As String = ""
         Property PinCode As String = ""
+        Property DOB As Date
         Property AadharNo As String = ""
-        Property Description As String = ""
-        Property TypeOfEngagement As String = ""
         Property TIN As String = ""
         Property CIN As String = ""
-        Property Partners As System.ComponentModel.BindingList(Of Partner)
         Property Type As String = ""
-        Property Credentials As System.ComponentModel.BindingList(Of Credential)
-        Property Jobs As System.ComponentModel.BindingList(Of Job)
+        Property Description As String = ""
+        Property TypeOfEngagement As String = ""
         Property Status As String = ""
-        Property Photo As Drawing.Image = Nothing
+        Property Partners As System.ComponentModel.BindingList(Of Partner)
+        Property Credentials As System.ComponentModel.BindingList(Of Credential)
+        Property Jobs As List(Of Job)
 
         Sub New(ByVal ID As Integer)
             Me.ID_ = ID
         End Sub
 
-        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal PAN As String, ByVal FatherName As String, ByVal Mobile As String, ByVal Email As String, ByVal DOB As String, ByVal AddressLine1 As String, ByVal AddressLine2 As String, ByVal District As String, ByVal PinCode As String, ByVal AadharNo As String, ByVal Description As String, ByVal TypeOfEngagement As String, ByVal TIN As String, ByVal CIN As String, ByVal Partners As System.ComponentModel.BindingList(Of Partner), ByVal Type As String, ByVal Credentials As System.ComponentModel.BindingList(Of Credential), ByVal Jobs As System.ComponentModel.BindingList(Of Job), ByVal Status As String, ByVal Photo As Drawing.Image)
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal PAN As String, ByVal FatherName As String, ByVal Mobile As String, ByVal Email As String, ByVal DOB As String, ByVal AddressLine1 As String, ByVal AddressLine2 As String, ByVal District As String, ByVal PinCode As String, ByVal AadharNo As String, ByVal Description As String, ByVal TypeOfEngagement As String, ByVal TIN As String, ByVal CIN As String, ByVal Partners As System.ComponentModel.BindingList(Of Partner), ByVal Type As String, ByVal Credentials As System.ComponentModel.BindingList(Of Credential), ByVal Jobs As List(Of Job), ByVal Status As String, ByVal Photo As Drawing.Image)
             Me.ID_ = ID
             Me.Name = Name
             Me.AadharNo = AadharNo
@@ -72,13 +78,6 @@ Namespace Objects
             Me.Type = Type
             Me.TypeOfEngagement = TypeOfEngagement
         End Sub
-
-        Dim ID_ As Integer = -1
-        ReadOnly Property ID As Integer
-            Get
-                Return ID_
-            End Get
-        End Property
 
         Public Overrides Function ToString() As String
             Return Name.ToString()
