84c6dcb0d8c2314a81c855b2f4554df4004c7ee3
Devil7DK <dineshthangavel47@gmail.com>
2018-12-26 13:55:07 +0530

    OMS: Add Separate Project for Utility - Estimate Bill Printer

---
+++ EstimateBillPrinter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Devil7 - Estimate Bill Printer")>
+<Assembly: AssemblyDescription("Utility to print & manage estimate bills to clients")>
+<Assembly: AssemblyCompany("Devil7 Softwares")>
+<Assembly: AssemblyProduct("Devil7 - Office Management System")>
+<Assembly: AssemblyCopyright("Copyright © Devil7 Softwares 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fdd5961b-e3cd-4705-90de-e793e2cedff9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
