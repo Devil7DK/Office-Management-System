377d1d8e1913d12cd4a92036b08cad0ef12859b3
Devil7DK <dineshthangavel47@gmail.com>
2019-01-18 17:15:37 +0530

    OMS Lib: Add Objects - Fees Reminder & Fees Item

---
+++ Devil7.Automation.OMS.Lib/Objects/FeesItem.vb
@@ -0,0 +1,58 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Imports System.ComponentModel
+
+Namespace Objects
+    Public Class FeesItem
+
+#Region "Properties/Fields"
+        <DisplayName("Date")>
+        Property Date_ As Date
+
+        <DisplayName("Detail")>
+        Property Name As String
+
+        <DisplayName("Effect")>
+        Property Effect As Enums.Effect
+
+        <DisplayName("Is Date Applicable")>
+        Property IsDateApplicable As Boolean
+#End Region
+
+#Region "Constructor"
+        Sub New()
+            Me.Date_ = Nothing
+            Me.IsDateApplicable = False
+            Me.Name = ""
+            Me.Effect = Enums.Effect.Dr
+        End Sub
+
+        Sub New(ByVal Date_ As Date, ByVal IsDateApplicable As Boolean, ByVal Name As String, ByVal Effect As Enums.Effect)
+            Me.Date_ = Date_
+            Me.IsDateApplicable = IsDateApplicable
+            Me.Name = Name
+            Me.Effect = Effect
+        End Sub
+#End Region
+
+    End Class
+End Namespace
---
+++ Devil7.Automation.OMS.Lib/Objects/FeesReminder.vb
@@ -0,0 +1,44 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Namespace Objects
+    Public Class FeesReminder
+
+#Region "Properties/Fields"
+        Property ID As Integer
+        Property Sender As Sender
+        Property Receiver As ClientMinimal
+        Property Items As List(Of FeesItem)
+        Property CustomText As String
+#End Region
+
+#Region "Constructor"
+        Sub New(ByVal ID As Integer, ByVal Sender As Sender, ByVal Receiver As ClientMinimal, ByVal Items As List(Of FeesItem), ByVal CustomText As String)
+            Me.ID = ID
+            Me.Sender = Sender
+            Me.Receiver = Receiver
+            Me.Items = Items
+            Me.CustomText = CustomText
+        End Sub
+#End Region
+
+    End Class
+End Namespace
