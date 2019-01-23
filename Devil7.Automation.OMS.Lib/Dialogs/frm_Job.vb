eec5f79a9fe8a644452b18de93bfdc8c7cf6592f
Devil7DK <dineshthangavel47@gmail.com>
2019-01-23 17:42:13 +0530

    OMS Lib: Job - Refactor Job Dialog

--- Devil7.Automation.OMS.Lib/Dialogs/frm_Job.vb
+++ Devil7.Automation.OMS.Lib/Dialogs/frm_Job.vb
@@ -20,58 +20,35 @@
 '=========================================================================='
 
 Imports System.Windows.Forms
-Imports Devil7.Automation.OMS.Lib
 Imports Devil7.Automation.OMS.Lib.Objects
 
 Namespace Dialogs
     Public Class frm_Job
+
+#Region "Variables"
         Dim Mode As Enums.DialogMode
-        Dim ID As Integer
-        Property Job As Job = Nothing
         Dim Users As New List(Of User)
-        Sub New(ByVal Mode As Enums.DialogMode, ByVal Groups As String(), ByVal SubGroups As String(), ByVal Users As List(Of User), Optional ByVal ID As Integer = -1)
+#End Region
+
+#Region "Properties"
+        Property Job As Job = Nothing
+#End Region
+
+#Region "Constructor"
+        Sub New(ByVal Mode As Enums.DialogMode, ByVal Groups As String(), ByVal SubGroups As String(), ByVal Users As List(Of User))
             InitializeComponent()
             Me.Mode = Mode
-            Me.ID = ID
-            On Error Resume Next
-            cmb_Group.Properties.Items.AddRange(Groups)
-            cmb_SubGroup.Properties.Items.AddRange(SubGroups)
             Me.Users = Users
-        End Sub
-        Private Sub btn_Template_Add_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Template_Add.Click
-            If OpenFileDialog1.ShowDialog = System.Windows.Forms.DialogResult.OK Then
-                lst_Templates.Items.AddRange(OpenFileDialog1.FileNames)
-            End If
-        End Sub
 
-        Private Sub btn_Template_Remove_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Template_Remove.Click
-            lst_Templates.Items.RemoveAt(lst_Templates.SelectedIndex)
+            If Groups IsNot Nothing AndAlso Groups.Count > 0 Then cmb_Group.Properties.Items.AddRange(Groups)
+            If SubGroups IsNot Nothing AndAlso SubGroups.Count > 0 Then cmb_SubGroup.Properties.Items.AddRange(SubGroups)
         End Sub
+#End Region
 
-        Private Sub btn_Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Cancel.Click
-            Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
-            Me.Close()
-        End Sub
-        Private Sub btn_Done_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Done.Click
-            Dim lstTMPL As New List(Of String)
-            For Each i As String In lst_Templates.Items
-                lstTMPL.Add(i)
-            Next
-            Dim lstSteps As New List(Of String)
-            lstSteps.AddRange(txt_Steps.Lines)
-            If Mode = Enums.DialogMode.Add Then
-                Job = Database.Jobs.AddNew(txt_Name.Text, cmb_Group.Text, cmb_Type.SelectedIndex, lstSteps, cmb_SubGroup.Text, lstTMPL, CType(gc_FollowUps.DataSource, List(Of Job)), CType(gc_AutoForwards.DataSource, List(Of AutoForward)), txt_NotifyInterval.Value, txt_DueInterval.Value, txt_PrimaryPeriod.SelectedIndex, True)
-            ElseIf Mode = Enums.DialogMode.Edit Then
-                Database.Jobs.Update(ID, txt_Name.Text, cmb_Group.Text, cmb_Type.SelectedIndex, lstSteps, cmb_SubGroup.Text, lstTMPL, CType(gc_FollowUps.DataSource, List(Of Job)), CType(gc_AutoForwards.DataSource, List(Of AutoForward)), txt_NotifyInterval.Value, txt_DueInterval.Value, txt_PrimaryPeriod.SelectedIndex, True)
-            End If
-            Me.DialogResult = System.Windows.Forms.DialogResult.OK
-            Me.Close()
-        End Sub
-
+#Region "Form Events"
         Private Sub frm_Job_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-            If Mode = Enums.DialogMode.Edit Then
+            If Mode = Enums.DialogMode.Edit AndAlso Job IsNot Nothing Then
                 Try
-                    Dim Job As Job = Database.Jobs.GetJobByID(ID, True)
                     txt_Name.Text = Job.Name
                     cmb_Group.Text = Job.Group
                     cmb_SubGroup.Text = Job.SubGroup
@@ -93,16 +70,56 @@ Namespace Dialogs
             Else
                 txt_PrimaryPeriod.SelectedIndex = 0
             End If
+            If gc_AutoForwards.DataSource IsNot Nothing Then gc_AutoForwards.DataSource = New List(Of AutoForward)
+            If gc_FollowUps.DataSource IsNot Nothing Then gc_FollowUps.DataSource = New List(Of Job)
             txt_Name.Focus()
+        End Sub
+#End Region
+
+#Region "Button Events"
+        Private Sub btn_Template_Add_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Template_Add.Click
+            If OpenFileDialog1.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                lst_Templates.Items.AddRange(OpenFileDialog1.FileNames)
+            End If
+        End Sub
+
+        Private Sub btn_Template_Remove_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Template_Remove.Click
+            lst_Templates.Items.RemoveAt(lst_Templates.SelectedIndex)
+        End Sub
+
+        Private Sub btn_Done_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Done.Click
+            Dim lstTMPL As New List(Of String)
+            For Each i As String In lst_Templates.Items
+                lstTMPL.Add(i)
+            Next
+
+            Dim lstSteps As New List(Of String)
+            lstSteps.AddRange(txt_Steps.Lines)
+
+            If Mode = Enums.DialogMode.Add Then
+                Job = Database.Jobs.AddNew(txt_Name.Text, cmb_Group.Text, cmb_Type.SelectedIndex, lstSteps, cmb_SubGroup.Text, lstTMPL, CType(gc_FollowUps.DataSource, List(Of Job)), CType(gc_AutoForwards.DataSource, List(Of AutoForward)), txt_NotifyInterval.Value, txt_DueInterval.Value, txt_PrimaryPeriod.SelectedIndex, True)
+            ElseIf Mode = Enums.DialogMode.Edit And Job IsNot Nothing Then
+                Database.Jobs.Update(Job.ID, txt_Name.Text, cmb_Group.Text, cmb_Type.SelectedIndex, lstSteps, cmb_SubGroup.Text, lstTMPL, CType(gc_FollowUps.DataSource, List(Of Job)), CType(gc_AutoForwards.DataSource, List(Of AutoForward)), txt_NotifyInterval.Value, txt_DueInterval.Value, txt_PrimaryPeriod.SelectedIndex, True)
+            Else
+                DevExpress.XtraEditors.XtraMessageBox.Show("Invalid Command!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+            Me.DialogResult = DialogResult.OK
+            Me.Close()
+        End Sub
+
+        Private Sub btn_Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Cancel.Click
+            Me.DialogResult = DialogResult.Cancel
+            Me.Close()
         End Sub
 
         Private Sub btn_FollowUps_Add_Click(sender As Object, e As EventArgs) Handles btn_FollowUps_Add.Click
             Dim d As New frm_Job_Lite(Enums.DialogMode.Add)
             If d.ShowDialog = System.Windows.Forms.DialogResult.OK Then
                 If gc_FollowUps.DataSource Is Nothing Then gc_FollowUps.DataSource = New List(Of Job)
-                If d.Job.ID = ID Then
+                If Job IsNot Nothing AndAlso d.Job.ID = Job.ID Then
                     DevExpress.XtraEditors.XtraMessageBox.Show("You can't add the job itself to its follow up...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-                ElseIf d.Job.FollowUps.Find(Function(c) c.ID = ID) IsNot Nothing Then
+                ElseIf Job IsNot Nothing AndAlso d.Job.FollowUps.Find(Function(c) c.ID = job.ID) IsNot Nothing Then
                     DevExpress.XtraEditors.XtraMessageBox.Show("You can't add a job which has the current job as its follow up...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
                 Else
                     CType(gc_FollowUps.DataSource, List(Of Job)).Add(d.Job)
@@ -114,8 +131,7 @@ Namespace Dialogs
         Private Sub btn_FollowUps_Remove_Click(sender As Object, e As EventArgs) Handles btn_FollowUps_Remove.Click
             If gv_FollowUps.SelectedRowsCount > 0 Then
                 For Each i As Integer In gv_FollowUps.GetSelectedRows
-                    Dim row As Integer = (i)
-                    Dim obj As Job = TryCast(gv_FollowUps.GetRow(row), Job)
+                    Dim obj As Job = TryCast(gv_FollowUps.GetRow(i), Job)
                     CType(gc_FollowUps.DataSource, List(Of Job)).Remove(obj)
                 Next
                 gc_FollowUps.RefreshDataSource()
@@ -134,12 +150,13 @@ Namespace Dialogs
         Private Sub btn_AutoForwards_Remove_Click(sender As Object, e As EventArgs) Handles btn_AutoForwards_Remove.Click
             If gv_AutoForwards.SelectedRowsCount > 0 Then
                 For Each i As Integer In gv_AutoForwards.GetSelectedRows
-                    Dim row As Integer = (i)
-                    Dim obj As AutoForward = TryCast(gv_AutoForwards.GetRow(row), AutoForward)
+                    Dim obj As AutoForward = TryCast(gv_AutoForwards.GetRow(i), AutoForward)
                     CType(gc_AutoForwards.DataSource, List(Of AutoForward)).Remove(obj)
                 Next
                 gc_AutoForwards.RefreshDataSource()
             End If
         End Sub
+#End Region
+
     End Class
 End Namespace
