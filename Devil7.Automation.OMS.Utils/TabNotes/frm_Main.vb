911f1f31f819c237895fe169244c475e3607c944
Devil7DK <dineshthangavel47@gmail.com>
2019-01-28 15:59:39 +0530

    Tab Notes: Initial Commit

---
+++ Devil7.Automation.OMS.Utils/TabNotes/frm_Main.vb
@@ -0,0 +1,7 @@
+﻿Public Class frm_Main
+    Private Sub TabFormControl1_OuterFormCreating(sender As Object, e As DevExpress.XtraBars.OuterFormCreatingEventArgs) Handles TabFormControl1.OuterFormCreating
+        Dim form As New frm_Main
+        form.TabFormControl.Pages.Clear()
+        e.Form = form
+    End Sub
+End Class
