d70078256c130b1486bef9a68dad2ca53a5efbb3
Devil7DK <dineshthangavel47@gmail.com>
2019-01-30 16:08:11 +0530

    Tab Notes: Add Custom Control for Viewing/Editing Notes

---
+++ Devil7.Automation.OMS.Utils/TabNotes/Controls/ExEditor.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExEditor
+    Inherits DevExpress.XtraEditors.XtraUserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ExEditor))
+        Me.BarManager1 = New DevExpress.XtraBars.BarManager()
+        Me.Bar2 = New DevExpress.XtraBars.Bar()
+        Me.btn_Save = New DevExpress.XtraBars.BarButtonItem()
+        Me.btn_Discard = New DevExpress.XtraBars.BarButtonItem()
+        Me.btn_Archive = New DevExpress.XtraBars.BarButtonItem()
+        Me.Bar3 = New DevExpress.XtraBars.Bar()
+        Me.txt_Status = New DevExpress.XtraBars.BarStaticItem()
+        Me.txt_Length = New DevExpress.XtraBars.BarStaticItem()
+        Me.barDockControlTop = New DevExpress.XtraBars.BarDockControl()
+        Me.barDockControlBottom = New DevExpress.XtraBars.BarDockControl()
+        Me.barDockControlLeft = New DevExpress.XtraBars.BarDockControl()
+        Me.barDockControlRight = New DevExpress.XtraBars.BarDockControl()
+        Me.txt_Content = New DevExpress.XtraEditors.MemoEdit()
+        Me.btn_EditTitle = New DevExpress.XtraBars.BarButtonItem()
+        CType(Me.BarManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txt_Content.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BarManager1
+        '
+        Me.BarManager1.Bars.AddRange(New DevExpress.XtraBars.Bar() {Me.Bar2, Me.Bar3})
+        Me.BarManager1.DockControls.Add(Me.barDockControlTop)
+        Me.BarManager1.DockControls.Add(Me.barDockControlBottom)
+        Me.BarManager1.DockControls.Add(Me.barDockControlLeft)
+        Me.BarManager1.DockControls.Add(Me.barDockControlRight)
+        Me.BarManager1.Form = Me
+        Me.BarManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.btn_Save, Me.btn_Discard, Me.txt_Status, Me.txt_Length, Me.btn_Archive, Me.btn_EditTitle})
+        Me.BarManager1.MainMenu = Me.Bar2
+        Me.BarManager1.MaxItemId = 7
+        Me.BarManager1.StatusBar = Me.Bar3
+        '
+        'Bar2
+        '
+        Me.Bar2.BarName = "Main menu"
+        Me.Bar2.DockCol = 0
+        Me.Bar2.DockRow = 0
+        Me.Bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top
+        Me.Bar2.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() {New DevExpress.XtraBars.LinkPersistInfo(Me.btn_Save), New DevExpress.XtraBars.LinkPersistInfo(Me.btn_Discard), New DevExpress.XtraBars.LinkPersistInfo(Me.btn_EditTitle, True), New DevExpress.XtraBars.LinkPersistInfo(Me.btn_Archive)})
+        Me.Bar2.OptionsBar.AllowQuickCustomization = False
+        Me.Bar2.OptionsBar.DrawDragBorder = False
+        Me.Bar2.OptionsBar.MultiLine = True
+        Me.Bar2.OptionsBar.UseWholeRow = True
+        Me.Bar2.Text = "Main menu"
+        '
+        'btn_Save
+        '
+        Me.btn_Save.Caption = "Save"
+        Me.btn_Save.Id = 0
+        Me.btn_Save.ImageOptions.SvgImage = CType(resources.GetObject("btn_Save.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_Save.Name = "btn_Save"
+        Me.btn_Save.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph
+        '
+        'btn_Discard
+        '
+        Me.btn_Discard.Caption = "Discard"
+        Me.btn_Discard.Id = 1
+        Me.btn_Discard.ImageOptions.SvgImage = CType(resources.GetObject("btn_Discard.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_Discard.Name = "btn_Discard"
+        Me.btn_Discard.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph
+        '
+        'btn_Archive
+        '
+        Me.btn_Archive.Caption = "Archive"
+        Me.btn_Archive.Id = 5
+        Me.btn_Archive.ImageOptions.SvgImage = CType(resources.GetObject("btn_Archive.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_Archive.Name = "btn_Archive"
+        Me.btn_Archive.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph
+        '
+        'Bar3
+        '
+        Me.Bar3.BarName = "Status bar"
+        Me.Bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom
+        Me.Bar3.DockCol = 0
+        Me.Bar3.DockRow = 0
+        Me.Bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom
+        Me.Bar3.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() {New DevExpress.XtraBars.LinkPersistInfo(Me.txt_Status), New DevExpress.XtraBars.LinkPersistInfo(Me.txt_Length)})
+        Me.Bar3.OptionsBar.AllowQuickCustomization = False
+        Me.Bar3.OptionsBar.DrawDragBorder = False
+        Me.Bar3.OptionsBar.UseWholeRow = True
+        Me.Bar3.Text = "Status bar"
+        '
+        'txt_Status
+        '
+        Me.txt_Status.Caption = "Unsaved"
+        Me.txt_Status.Id = 2
+        Me.txt_Status.Name = "txt_Status"
+        '
+        'txt_Length
+        '
+        Me.txt_Length.Caption = "Length : 0"
+        Me.txt_Length.Id = 4
+        Me.txt_Length.Name = "txt_Length"
+        '
+        'barDockControlTop
+        '
+        Me.barDockControlTop.CausesValidation = False
+        Me.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top
+        Me.barDockControlTop.Location = New System.Drawing.Point(0, 0)
+        Me.barDockControlTop.Manager = Me.BarManager1
+        Me.barDockControlTop.Size = New System.Drawing.Size(433, 24)
+        '
+        'barDockControlBottom
+        '
+        Me.barDockControlBottom.CausesValidation = False
+        Me.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.barDockControlBottom.Location = New System.Drawing.Point(0, 320)
+        Me.barDockControlBottom.Manager = Me.BarManager1
+        Me.barDockControlBottom.Size = New System.Drawing.Size(433, 25)
+        '
+        'barDockControlLeft
+        '
+        Me.barDockControlLeft.CausesValidation = False
+        Me.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left
+        Me.barDockControlLeft.Location = New System.Drawing.Point(0, 24)
+        Me.barDockControlLeft.Manager = Me.BarManager1
+        Me.barDockControlLeft.Size = New System.Drawing.Size(0, 296)
+        '
+        'barDockControlRight
+        '
+        Me.barDockControlRight.CausesValidation = False
+        Me.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right
+        Me.barDockControlRight.Location = New System.Drawing.Point(433, 24)
+        Me.barDockControlRight.Manager = Me.BarManager1
+        Me.barDockControlRight.Size = New System.Drawing.Size(0, 296)
+        '
+        'txt_Content
+        '
+        Me.txt_Content.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txt_Content.Location = New System.Drawing.Point(0, 24)
+        Me.txt_Content.MenuManager = Me.BarManager1
+        Me.txt_Content.Name = "txt_Content"
+        Me.txt_Content.Size = New System.Drawing.Size(433, 296)
+        Me.txt_Content.TabIndex = 10
+        '
+        'btn_EditTitle
+        '
+        Me.btn_EditTitle.Caption = "Edit Title"
+        Me.btn_EditTitle.Id = 6
+        Me.btn_EditTitle.ImageOptions.SvgImage = CType(resources.GetObject("btn_EditTitle.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_EditTitle.Name = "btn_EditTitle"
+        Me.btn_EditTitle.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph
+        '
+        'ExEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.txt_Content)
+        Me.Controls.Add(Me.barDockControlLeft)
+        Me.Controls.Add(Me.barDockControlRight)
+        Me.Controls.Add(Me.barDockControlBottom)
+        Me.Controls.Add(Me.barDockControlTop)
+        Me.Name = "ExEditor"
+        Me.Size = New System.Drawing.Size(433, 345)
+        CType(Me.BarManager1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txt_Content.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BarManager1 As DevExpress.XtraBars.BarManager
+    Friend WithEvents Bar2 As DevExpress.XtraBars.Bar
+    Friend WithEvents Bar3 As DevExpress.XtraBars.Bar
+    Friend WithEvents barDockControlTop As DevExpress.XtraBars.BarDockControl
+    Friend WithEvents barDockControlBottom As DevExpress.XtraBars.BarDockControl
+    Friend WithEvents barDockControlLeft As DevExpress.XtraBars.BarDockControl
+    Friend WithEvents barDockControlRight As DevExpress.XtraBars.BarDockControl
+    Friend WithEvents btn_Save As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents btn_Discard As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents txt_Status As DevExpress.XtraBars.BarStaticItem
+    Friend WithEvents txt_Length As DevExpress.XtraBars.BarStaticItem
+    Friend WithEvents btn_Archive As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents txt_Content As DevExpress.XtraEditors.MemoEdit
+    Friend WithEvents btn_EditTitle As DevExpress.XtraBars.BarButtonItem
+End Class
---
+++ Devil7.Automation.OMS.Utils/TabNotes/Controls/ExEditor.vb
@@ -0,0 +1,142 @@
+﻿Imports DevExpress.XtraBars
+Imports Devil7.Automation.OMS.Lib
+
+Public Class ExEditor
+
+#Region "Variables"
+    Dim Loaded As Boolean = False
+    Dim TmpTitle As String
+    Dim User As Objects.User
+
+    Dim Mode As Enums.DialogMode = Enums.DialogMode.Add
+    Dim TabPage As TabFormPage
+    Dim TabControl As TabFormControl
+#End Region
+
+#Region "Properties/Fields"
+    WithEvents Note_ As Objects.Note
+    Property Note As Objects.Note
+        Get
+            Return Note_
+        End Get
+        Set(value As Objects.Note)
+            Me.Note_ = value
+            Me.RefreshData()
+        End Set
+    End Property
+#End Region
+
+#Region "Constructor"
+    Sub New(ByVal Note As Objects.Note, ByVal TabPage As TabFormPage, ByVal TabControl As TabFormControl)
+        InitializeComponent()
+
+        Me.Note = New Objects.Note(Note.ID, Note.Title, Note.Content, Note.DateAdded, Note.DateEdited)
+        Me.Mode = Enums.DialogMode.Edit
+        Me.TabPage = TabPage
+        Me.TabControl = TabControl
+
+        RefreshData()
+    End Sub
+
+    Sub New(ByVal User As Objects.User, ByVal Title As String, ByVal TabPage As TabFormPage, ByVal TabControl As TabFormControl)
+        InitializeComponent()
+
+        Me.User = User
+        Me.TmpTitle = Title
+        Me.Mode = Enums.DialogMode.Add
+        Me.TabPage = TabPage
+        Me.TabControl = TabControl
+
+        If TabPage IsNot Nothing Then TabPage.Text = TmpTitle
+    End Sub
+#End Region
+
+#Region "Subs"
+    Public Sub RefreshData()
+        If Me.Note IsNot Nothing Then
+            Loaded = False
+            txt_Content.Text = Me.Note.Content
+            Loaded = True
+
+            If Me.TabPage IsNot Nothing Then
+                Me.TabPage.Text = Me.Note.Title
+            End If
+
+            If Note.Saved Then
+                txt_Status.Caption = "Saved"
+            Else
+                txt_Status.Caption = "Unsaved"
+            End If
+
+            Me.Mode = Enums.DialogMode.Edit
+        End If
+    End Sub
+#End Region
+
+#Region "Button Events"
+    Private Sub btn_Save_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_Save.ItemClick
+        If Mode = Enums.DialogMode.Add Then
+            Dim Note As Objects.Note = Database.Notes.AddNew(TmpTitle, txt_Content.Text, User)
+            If Note IsNot Nothing Then
+                Me.Note = Note
+                Me.Mode = Enums.DialogMode.Edit
+                DevExpress.XtraEditors.XtraMessageBox.Show("Successfully Saved New Note.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Else
+            Me.Note.Save(User)
+            DevExpress.XtraEditors.XtraMessageBox.Show("Successfully Saved Changes to the Note.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btn_Discard_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_Discard.ItemClick
+        If Mode = Enums.DialogMode.Edit AndAlso Me.Note IsNot Nothing Then
+            If DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure..? Do you want to discard this Note...? This cannot be undone!", "Sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+                If Database.Notes.Remove(Me.Note.ID) Then
+                    If TabControl IsNot Nothing AndAlso TabPage IsNot Nothing Then TabControl.Pages.Remove(TabPage)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_Archive_ItemClick(sender As Object, e As ItemClickEventArgs) Handles btn_Archive.ItemClick
+        If Mode = Enums.DialogMode.Edit AndAlso Me.Note IsNot Nothing Then
+            If Database.Notes.Update(Me.Note.ID, True) Then
+                If TabControl IsNot Nothing AndAlso TabPage IsNot Nothing Then TabControl.Pages.Remove(TabPage)
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_EditTitle_ItemClick(sender As Object, e As ItemClickEventArgs) Handles btn_EditTitle.ItemClick
+        If Mode = Enums.DialogMode.Edit AndAlso Me.Note IsNot Nothing Then
+            Dim Title As String = Utils.ExInputBox("Change Title", "Enter New Title for Note :", Me.Note.Title)
+            If Not String.IsNullOrEmpty(Title) Then
+                Me.Note.UpdateTitle(Title)
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Other Events"
+    Private Sub Note_SavedStatusChanged(sender As Object, Saved As Boolean) Handles Note_.SavedStatusChanged
+        If Saved Then
+            txt_Status.Caption = "Saved"
+        Else
+            txt_Status.Caption = "Unsaved"
+        End If
+    End Sub
+
+    Private Sub Note_TitleChanged(sender As Object, Title As String) Handles Note_.TitleChanged
+        If TabPage IsNot Nothing Then
+            TabPage.Text = Title
+        End If
+    End Sub
+
+    Private Sub txt_Content_EditValueChanged(sender As Object, e As EventArgs) Handles txt_Content.EditValueChanged
+        txt_Length.Caption = String.Format("Length : {0}", txt_Content.Text.Length)
+        If Loaded Then
+            If Me.Note IsNot Nothing Then Me.Note.Content = txt_Content.Text
+        End If
+    End Sub
+#End Region
+
+End Class
