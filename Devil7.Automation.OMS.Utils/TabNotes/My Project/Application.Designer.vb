653a87c37d25cfa88bb2b08b5acd08ff1203579a
Devil7DK <dineshthangavel47@gmail.com>
2019-01-30 16:05:07 +0530

    Tab Notes: Add Required Properties & Set Compile Options

--- Devil7.Automation.OMS.Utils/TabNotes/My Project/Application.Designer.vb
+++ Devil7.Automation.OMS.Utils/TabNotes/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TabNotes.frm_Main
+            Me.MainForm = Global.TabNotes.frm_Login
         End Sub
     End Class
 End Namespace
