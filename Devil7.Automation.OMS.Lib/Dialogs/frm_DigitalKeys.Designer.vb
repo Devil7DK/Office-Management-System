cb0d9c66bb473ea0e8e1531cf3916d262f8c4f83
Devil7DK <dineshthangavel47@gmail.com>
2019-05-18 10:45:44 +0530

    OMS Lib: Add Dialog to View/Add/Edit Digital Key Details

---
+++ Devil7.Automation.OMS.Lib/Dialogs/frm_DigitalKeys.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_DigitalKeys
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_DigitalKeys))
+        Me.gc_DigitalKeys = New DevExpress.XtraGrid.GridControl()
+        Me.gv_DigitalKeys = New DevExpress.XtraGrid.Views.Grid.GridView()
+        CType(Me.gc_DigitalKeys, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gv_DigitalKeys, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gc_DigitalKeys
+        '
+        Me.gc_DigitalKeys.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gc_DigitalKeys.Location = New System.Drawing.Point(0, 0)
+        Me.gc_DigitalKeys.MainView = Me.gv_DigitalKeys
+        Me.gc_DigitalKeys.Name = "gc_DigitalKeys"
+        Me.gc_DigitalKeys.Size = New System.Drawing.Size(634, 382)
+        Me.gc_DigitalKeys.TabIndex = 0
+        Me.gc_DigitalKeys.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gv_DigitalKeys})
+        '
+        'gv_DigitalKeys
+        '
+        Me.gv_DigitalKeys.GridControl = Me.gc_DigitalKeys
+        Me.gv_DigitalKeys.Name = "gv_DigitalKeys"
+        Me.gv_DigitalKeys.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.[True]
+        Me.gv_DigitalKeys.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.[True]
+        Me.gv_DigitalKeys.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Bottom
+        '
+        'frm_DigitalKeys
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(634, 382)
+        Me.Controls.Add(Me.gc_DigitalKeys)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimizeBox = False
+        Me.Name = "frm_DigitalKeys"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Digital Keys Register"
+        CType(Me.gc_DigitalKeys, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gv_DigitalKeys, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gc_DigitalKeys As DevExpress.XtraGrid.GridControl
+    Friend WithEvents gv_DigitalKeys As DevExpress.XtraGrid.Views.Grid.GridView
+End Class
---
+++ Devil7.Automation.OMS.Lib/Dialogs/frm_DigitalKeys.vb
@@ -0,0 +1,132 @@
+﻿Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports DevExpress.XtraGrid.Views.Base
+Imports DevExpress.XtraGrid.Views.Grid
+Imports Devil7.Automation.OMS.Lib
+
+Public Class frm_DigitalKeys
+
+#Region "Variables"
+    Dim ProgressOverlayHandle As DevExpress.XtraSplashScreen.IOverlaySplashScreenHandle
+    Dim Edited As Boolean = False
+    Dim Clients As List(Of Objects.Client)
+#End Region
+
+#Region "Properties"
+    Property DigitalKeys As BindingList(Of Objects.DigitalKeyInfo)
+        Get
+            If gc_DigitalKeys.DataSource Is Nothing Then
+                gc_DigitalKeys.DataSource = New BindingList(Of Objects.DigitalKeyInfo) With {.AllowNew = True, .AllowEdit = True, .AllowRemove = True}
+            End If
+
+            Return CType(gc_DigitalKeys.DataSource, BindingList(Of Objects.DigitalKeyInfo))
+        End Get
+        Set(value As BindingList(Of Objects.DigitalKeyInfo))
+            Me.Invoke(Sub()
+                          gc_DigitalKeys.DataSource = value
+                          gc_DigitalKeys.RefreshDataSource()
+                      End Sub)
+        End Set
+    End Property
+#End Region
+
+#Region "Constructor"
+    Sub New(ByVal Clients As List(Of Objects.Client))
+        InitializeComponent()
+
+        Me.Clients = Clients
+    End Sub
+#End Region
+
+#Region "Subs"
+    Sub ShowProgressOverlay()
+        ProgressOverlayHandle = DevExpress.XtraSplashScreen.SplashScreenManager.ShowOverlayForm(Me)
+    End Sub
+
+    Sub CloseProgressOverlay()
+        If ProgressOverlayHandle IsNot Nothing Then DevExpress.XtraSplashScreen.SplashScreenManager.CloseOverlayForm(ProgressOverlayHandle)
+    End Sub
+#End Region
+
+#Region "Form Events"
+    Private Async Sub frm_DigitalKeys_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        ShowProgressOverlay()
+        Dim DigitalKeys As New BindingList(Of Objects.DigitalKeyInfo) With {.AllowNew = True, .AllowEdit = True, .AllowRemove = True}
+        Try
+            Await Threading.Tasks.Task.Run(Sub()
+                                               DigitalKeys = New BindingList(Of Objects.DigitalKeyInfo)(Database.DigitalKeyInfos.GetAll(Clients, False))
+                                           End Sub)
+        Catch ex As Exception
+            DevExpress.XtraEditors.XtraMessageBox.Show("Unable to load DigitalKeys data!" & vbNewLine & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Invoke(Sub()
+                       DialogResult = DialogResult.Cancel
+                       Close()
+                   End Sub)
+        End Try
+        Me.DigitalKeys = DigitalKeys
+        CloseProgressOverlay()
+    End Sub
+
+    Private Sub frm_DigitalKeys_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If Edited Then
+            DialogResult = DialogResult.OK
+        Else
+            DialogResult = DialogResult.Cancel
+        End If
+    End Sub
+#End Region
+
+#Region "Grid Events"
+    Private Async Sub gv_DigitalKeys_RowUpdated(sender As Object, e As RowObjectEventArgs) Handles gv_DigitalKeys.RowUpdated
+        ShowProgressOverlay()
+        Await Threading.Tasks.Task.Run(Sub()
+                                           Dim Row As Objects.DigitalKeyInfo = CType(e.Row, Objects.DigitalKeyInfo)
+                                           If e.RowHandle = DevExpress.XtraGrid.GridControl.NewItemRowHandle Then
+                                               Database.DigitalKeyInfos.AddNew(Row)
+                                               Edited = True
+                                               Invoke(Sub() gc_DigitalKeys.RefreshDataSource())
+                                           Else
+                                               Database.DigitalKeyInfos.Update(Row)
+                                               Edited = True
+                                               Invoke(Sub() gc_DigitalKeys.RefreshDataSource())
+                                           End If
+                                       End Sub)
+        CloseProgressOverlay()
+    End Sub
+
+    Private Async Sub gv_DigitalKeys_KeyDown(sender As Object, e As KeyEventArgs) Handles gv_DigitalKeys.KeyDown
+        ShowProgressOverlay()
+        Await Threading.Tasks.Task.Run(Sub()
+                                           If e.KeyCode = Keys.Delete AndAlso gv_DigitalKeys.SelectedRowsCount > 0 Then
+                                               For Each i As Integer In gv_DigitalKeys.GetSelectedRows
+                                                   Dim Item As Objects.DigitalKeyInfo = CType(gv_DigitalKeys.GetRow(i), Objects.DigitalKeyInfo)
+                                                   Database.DigitalKeyInfos.Remove(CInt(Item.ID))
+                                                   Invoke(Sub() DigitalKeys.Remove(Item))
+                                                   Edited = True
+                                               Next
+                                               Invoke(Sub() gc_DigitalKeys.RefreshDataSource())
+                                           End If
+                                       End Sub)
+        CloseProgressOverlay()
+    End Sub
+
+    Private Sub gv_DigitalKeys_CustomRowCellEdit(sender As Object, e As CustomRowCellEditEventArgs) Handles gv_DigitalKeys.CustomRowCellEdit
+        If e.Column.FieldName = "Client" Then
+            Dim Editor As New DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit
+            Editor.DataSource = Clients
+            Editor.PopulateColumns()
+
+            Dim VisibleColumns As String() = {"PAN", "ClientName", "Name", "TradeName"}
+            For Each Column As DevExpress.XtraEditors.Controls.LookUpColumnInfo In Editor.Columns
+                If VisibleColumns.Contains(Column.FieldName) Then
+                    Column.Visible = True
+                Else
+                    Column.Visible = False
+                End If
+            Next
+            e.RepositoryItem = Editor
+        End If
+    End Sub
+#End Region
+
+End Class
