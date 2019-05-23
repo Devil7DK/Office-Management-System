af67347f33c22155413935fbb4fcf8bd3ab40432
Devil7DK <dineshthangavel47@gmail.com>
2019-05-23 15:12:04 +0530

    OMS Lib: Add Digital Keys Alert Dialog

---
+++ Devil7.Automation.OMS.Lib/Dialogs/frm_DigitalKeysAlert.Designer.vb
@@ -0,0 +1,82 @@
+﻿Namespace Dialogs
+    <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+    Partial Class frm_DigitalKeysAlert
+        Inherits DevExpress.XtraEditors.XtraForm
+
+        'Form overrides dispose to clean up the component list.
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        'Required by the Windows Form Designer
+        Private components As System.ComponentModel.IContainer
+
+        'NOTE: The following procedure is required by the Windows Form Designer
+        'It can be modified using the Windows Form Designer.  
+        'Do not modify it using the code editor.
+        <System.Diagnostics.DebuggerStepThrough()>
+        Private Sub InitializeComponent()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_DigitalKeysAlert))
+            Me.gc_DigitalKeys = New DevExpress.XtraGrid.GridControl()
+            Me.gv_DigitalKeys = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.lbl_Message = New DevExpress.XtraEditors.LabelControl()
+            CType(Me.gc_DigitalKeys, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gv_DigitalKeys, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            'gc_DigitalKeys
+            '
+            Me.gc_DigitalKeys.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gc_DigitalKeys.Location = New System.Drawing.Point(0, 38)
+            Me.gc_DigitalKeys.MainView = Me.gv_DigitalKeys
+            Me.gc_DigitalKeys.Name = "gc_DigitalKeys"
+            Me.gc_DigitalKeys.Size = New System.Drawing.Size(472, 304)
+            Me.gc_DigitalKeys.TabIndex = 0
+            Me.gc_DigitalKeys.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gv_DigitalKeys})
+            '
+            'gv_DigitalKeys
+            '
+            Me.gv_DigitalKeys.GridControl = Me.gc_DigitalKeys
+            Me.gv_DigitalKeys.Name = "gv_DigitalKeys"
+            '
+            'lbl_Message
+            '
+            Me.lbl_Message.Appearance.Options.UseTextOptions = True
+            Me.lbl_Message.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap
+            Me.lbl_Message.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None
+            Me.lbl_Message.Dock = System.Windows.Forms.DockStyle.Top
+            Me.lbl_Message.Location = New System.Drawing.Point(0, 0)
+            Me.lbl_Message.Name = "lbl_Message"
+            Me.lbl_Message.Padding = New System.Windows.Forms.Padding(10, 0, 10, 0)
+            Me.lbl_Message.Size = New System.Drawing.Size(472, 38)
+            Me.lbl_Message.TabIndex = 1
+            Me.lbl_Message.Text = "The following digtal keys must be renewed! Kindly take action regarding these dig" &
+    "ital keys && update the status"
+            '
+            'frm_DigitalKeysAlert
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(472, 342)
+            Me.Controls.Add(Me.gc_DigitalKeys)
+            Me.Controls.Add(Me.lbl_Message)
+            Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+            Me.MinimizeBox = False
+            Me.Name = "frm_DigitalKeysAlert"
+            Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+            Me.Text = "Digital Keys"
+            CType(Me.gc_DigitalKeys, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gv_DigitalKeys, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        Friend WithEvents gc_DigitalKeys As DevExpress.XtraGrid.GridControl
+        Friend WithEvents gv_DigitalKeys As DevExpress.XtraGrid.Views.Grid.GridView
+        Friend WithEvents lbl_Message As DevExpress.XtraEditors.LabelControl
+    End Class
+End Namespace
---
+++ Devil7.Automation.OMS.Lib/Dialogs/frm_DigitalKeysAlert.vb
@@ -0,0 +1,69 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Namespace Dialogs
+    Public Class frm_DigitalKeysAlert
+
+#Region "Consturctor"
+        Sub New(ByVal DigitalKeys As List(Of Objects.DigitalKeyInfo))
+            InitializeComponent()
+
+            gc_DigitalKeys.DataSource = DigitalKeys
+            Utils.Misc.MakeColumnsFilterable(gv_DigitalKeys)
+            Utils.Misc.FitColumnsToContent(gv_DigitalKeys)
+        End Sub
+#End Region
+
+#Region "Events"
+        Private Sub gv_DigitalKeys_RowCellStyle(sender As Object, e As DevExpress.XtraGrid.Views.Grid.RowCellStyleEventArgs) Handles gv_DigitalKeys.RowCellStyle
+            If e.Column.FieldName = "DaysToExpire" Then
+                If e.CellValue IsNot Nothing Then
+                    Try
+                        Dim Value As Integer = CInt(e.CellValue)
+                        e.Appearance.BackColor = Utils.Misc.ColorLerp(Drawing.Color.FromArgb(255, 255, 160, 160), Drawing.Color.LightGreen, If(Value > 365, 1, (Value / 365)))
+                    Catch ex As Exception
+
+                    End Try
+                End If
+            ElseIf e.Column.FieldName = "Validity" Then
+                If e.CellValue IsNot Nothing Then
+                    Try
+                        Dim Value As Enums.DigitalKeyValidity = CInt(e.CellValue)
+                        Select Case Value
+                            Case Enums.DigitalKeyValidity.Valid
+                                e.Appearance.BackColor = Drawing.Color.LightGreen
+                            Case Enums.DigitalKeyValidity.ExpiringSoon
+                                e.Appearance.BackColor = Drawing.Color.LightYellow
+                            Case Enums.DigitalKeyValidity.ExpiringToday
+                                e.Appearance.BackColor = Drawing.Color.FromArgb(255, 255, 80, 80)
+                            Case Enums.DigitalKeyValidity.Expired
+                                e.Appearance.BackColor = Drawing.Color.FromArgb(255, 255, 160, 160)
+                        End Select
+                    Catch ex As Exception
+
+                    End Try
+                End If
+            End If
+        End Sub
+#End Region
+
+    End Class
+End Namespace
